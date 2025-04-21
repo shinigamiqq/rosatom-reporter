99c77a2d4786eec6a614656daef2f6d4b83424f9
alex_mcqueen <mitiacc437@gmail.com>
2025-04-21 18:41:16 +0300

    extension update for hotel checks method

--- server/png_documents_validator/main.py
+++ server/png_documents_validator/main.py
@@ -58,35 +58,38 @@ async def post_png_air_documents(file: UploadFile = File(...)):
 
 @app.post('/hotel_checks')
 async def post_png_hotel_checks(file: UploadFile = File(...)):
-    image = Image.open(io.BytesIO(await file.read()))
-    image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    qr_codes = decode(image)
-    for qr in qr_codes:
-        qr_data = qr.data.decode('utf-8')
-        print("QR-data:", qr_data)
+    extenstion_match = re.search(r"\b.(jpg|png|jpeg)\b", file.filename)
+    extension = extenstion_match.group(0) if extenstion_match else "Некорректное расширение файла"
+    if extension == ".jpg" or extension == ".png" or extension == ".jpeg":
+        image = Image.open(io.BytesIO(await file.read()))
+        image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        qr_codes = decode(image)
+        for qr in qr_codes:
+            qr_data = qr.data.decode('utf-8')
+            print("QR-data:", qr_data)
 
-        date_match = re.search(r't=(\d{4})(\d{2})(\d{2})T', qr_data)
-        price_match = re.search(r's=([\d\.]+)', qr_data)
+            date_match = re.search(r't=(\d{4})(\d{2})(\d{2})T', qr_data)
+            price_match = re.search(r's=([\d\.]+)', qr_data)
 
-        price = price_match.group(1) if price_match else "Цена не найдена"
+            price = price_match.group(1) if price_match else "Цена не найдена"
 
-        if date_match:
-            date = f"{date_match.group(3)}.{date_match.group(2)}.{date_match.group(1)}"
-        else:
-            date = "Дата не найдена"
+            if date_match:
+                date = f"{date_match.group(3)}.{date_match.group(2)}.{date_match.group(1)}"
+            else:
+                date = "Дата не найдена"
 
-        print(f"Дата: {date}")
-        '''
-        rect_points = qr.polygon
-        if len(rect_points) == 4:
-            pts = np.array(rect_points, dtype=np.int32)
-            pts = pts.reshape((-1, 1, 2))
-            cv2.polylines(image_cv, [pts], True, (0, 255, 0), 2)
-        else:
-            cv2.rectangle(image_cv, (qr.rect.left, qr.rect.top), 
-                          (qr.rect.left + qr.rect.width, qr.rect.top + qr.rect.height), 
-                          (0, 255, 0), 2)
+            print(f"Дата: {date}")
+            '''
+            rect_points = qr.polygon
+            if len(rect_points) == 4:
+                pts = np.array(rect_points, dtype=np.int32)
+                pts = pts.reshape((-1, 1, 2))
+                cv2.polylines(image_cv, [pts], True, (0, 255, 0), 2)
+            else:
+                cv2.rectangle(image_cv, (qr.rect.left, qr.rect.top), 
+                              (qr.rect.left + qr.rect.width, qr.rect.top + qr.rect.height), 
+                              (0, 255, 0), 2)
 
-        cv2.imwrite("processed_photo.png", image_cv)
-        '''
-        return {"Date": date, "Price": price}
+            cv2.imwrite("processed_photo.png", image_cv)
+            '''
+            return {"Date": date, "Price": price}
